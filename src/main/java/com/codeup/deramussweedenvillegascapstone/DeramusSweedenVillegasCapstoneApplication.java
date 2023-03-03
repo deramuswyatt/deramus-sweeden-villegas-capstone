e849b0161af1ba965e61bc12e0bb7cb56d32f050
Ernesto Villegas <ernesto.nmn.villegas@gmail.com>
2023-03-03 12:24:15 -0600

    First commit

---
+++ src/main/java/com/codeup/deramussweedenvillegascapstone/DeramusSweedenVillegasCapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.deramussweedenvillegascapstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeramusSweedenVillegasCapstoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeramusSweedenVillegasCapstoneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/deramussweedenvillegascapstone/DeramusSweedenVillegasCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.deramussweedenvillegascapstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeramusSweedenVillegasCapstoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
