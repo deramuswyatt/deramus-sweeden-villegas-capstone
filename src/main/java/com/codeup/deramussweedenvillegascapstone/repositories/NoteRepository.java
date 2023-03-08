cb0306b29be9ccb09e02c016cbba2db40fd06a6d
Ernesto Villegas <ernesto.nmn.villegas@gmail.com>
2023-03-07 19:25:08 -0600

    prop and user controller create working

--- src/main/java/com/codeup/deramussweedenvillegascapstone/repositories/NoteRepository.java
+++ src/main/java/com/codeup/deramussweedenvillegascapstone/repositories/NoteRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface NoteRepository extends JpaRepository<Note, Long> {
     Note findNotesById(long id);
 
-    Note searchAllByTitleOrBodyOrPropertyZipContaining(String query);
 }
