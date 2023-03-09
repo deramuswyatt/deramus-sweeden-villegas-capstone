2d9723e35b20fb537585ab09f32a9880588a16d9
Ernesto Villegas <ernesto.nmn.villegas@gmail.com>
2023-03-09 14:52:42 -0600

    notes crud and partials

--- src/main/java/com/codeup/deramussweedenvillegascapstone/Controllers/NoteController.java
+++ src/main/java/com/codeup/deramussweedenvillegascapstone/Controllers/NoteController.java
@@ -6,6 +6,7 @@ import com.codeup.deramussweedenvillegascapstone.models.User;
 import com.codeup.deramussweedenvillegascapstone.repositories.NoteRepository;
 import com.codeup.deramussweedenvillegascapstone.repositories.PropertyRepository;
 import com.codeup.deramussweedenvillegascapstone.repositories.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -45,7 +46,7 @@ public class NoteController {
 //        Property property = propDao.findPropertiesById(id);
         note.setProperty(property);
         noteDao.save(note);
-        return "redirect:/current-weather";
+        return "redirect:/notes";
     }
 
 
@@ -54,6 +55,31 @@ public class NoteController {
         model.addAttribute("notes", noteDao.searchByNoteLike(query));
         return "notes/index";
     }
+
+
+
+    @GetMapping("/notes/{id}")
+    public String indNote(@PathVariable long id, Model model) {
+        model.addAttribute("notes", noteDao.findNotesById(id));
+        model.addAttribute("notes", noteDao.findNotesById(id));
+        return "notes/show";
+    }
+
+
+
+
+    @GetMapping("/notes/{id}/edit")
+    public String editNoteForm(@PathVariable long id, Model model){
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Property property = propDao.findPropertiesById(id);
+        if (user.getId() == property.getUser().getId()) {
+            model.addAttribute("props", property);
+            return "posts/create";
+        } else {
+            return "redirect:/posts";
+        }
+    }
+
 
 
 }
